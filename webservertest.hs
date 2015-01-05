8239c8b76693621aa08c633f3290a9f575bc5b6a
Avi Weinstock <aweinstock314@gmail.com>
2015-01-05 16:40:03 -0500

    Changed some string concatenation to printf in webservertest.

--- webservertest.hs
+++ webservertest.hs
@@ -5,6 +5,7 @@ import Network
 import Network.HTTP.Base
 import Network.HTTP.Stream
 import System.IO
+import Text.Printf
 
 instance Stream Handle where
     readLine = liftM Right . (liftM $ flip (++) "\n") . hGetLine
@@ -16,9 +17,9 @@ instance Stream Handle where
 helloResponse = Response{rspCode=(2,0,0), rspReason="OK", rspHeaders=[], rspBody="Hello, world!"}
 
 processRequest (handle, host, port) = do
-    putStrLn $ "Accepted a connection from " ++ host ++ ":" ++ show port
+    printf "Accepted a connection from %s:%s\n" host $ show port
     request <- receiveHTTP handle
-    putStrLn $ "Received " ++ (show . show) request ++ " from " ++ host ++ ":" ++ show port
+    printf "Received %s from %s:%s\n" (show $ show request) host $ show port
     respondHTTP handle helloResponse
     hClose handle
 
