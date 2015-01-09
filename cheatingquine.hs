51e37889ad40825823b1ccc9b5b5b94bbeb10ad1
Avi Weinstock <aweinstock314@gmail.com>
2015-01-09 12:04:47 -0500

    Revised the Template Haskell expression in cheatingquine.hs to a more pointfree resembling form.

--- cheatingquine.hs
+++ cheatingquine.hs
@@ -1,8 +1,9 @@
+-- strings ./cheatingquine | grep -A $(./cheatingquine | wc -l) 'grep -A'
 {-# LANGUAGE TemplateHaskell, CPP #-}
 import Language.Haskell.TH
 {- Note: this reads the source at compile time, not at 
 runtime; so if the source is modified after compiling, the 
 compiled version outputs the unmodified source (until it's 
 recompiled) -}
-sourceCode = $(return (readFile __FILE__) >>= runIO >>= stringE)
+sourceCode = $(runIO . fmap (LitE . StringL) $ readFile __FILE__)
 main = putStr sourceCode
