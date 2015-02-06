0b3fee9ea21876eebf3baf1d71b740ae1dc7497d
Avi Weinstock <aweinstock314@gmail.com>
2015-02-05 19:18:32 -0500

    Changed the display of the heartbeat messages to use a table as a 2d ringbuffer. Added numbers to the pulse messages.

--- webservertest2.hs
+++ webservertest2.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad
+import Data.Char
 import Data.Monoid
 import Data.String
 import Language.Javascript.JMacro
@@ -15,6 +16,7 @@ import qualified Text.Blaze.Html5.Attributes as A
 import qualified Text.Blaze.Internal
 import qualified Text.PrettyPrint.Leijen.Text as R
 
+lbs = L.pack . map (fromIntegral.ord)
 delayMs = threadDelay . (*) 1000
 
 onloadDo :: (JsToDoc a, JMacro a, Text.Blaze.Internal.Attributable c) => c -> a -> c
@@ -24,10 +26,11 @@ embedScript, showScript :: (JsToDoc a, JMacro a) => a -> H.Markup
 embedScript = H.script . H.string . show . R.renderOneLine . renderJs
 showScript = H.pre . H.string . show . renderJs
 
-factorialsDiv, escapingDiv, heartBeatDiv :: IsString a => a
+factorialsDiv, escapingDiv, heartBeatDiv, heartBeatTable :: IsString a => a
 factorialsDiv = "factorialsDiv"
 escapingDiv = "escapingDiv"
 heartBeatDiv = "heartBeatDiv"
+heartBeatTable = "heartBeatTable"
 
 defineFactorial = [jmacro|
 function !factorial(n) {
@@ -57,17 +60,41 @@ heartBeatServerUrl = "ws://localhost:8501"
 heartBeatServer portNumber = WS.runServer "0.0.0.0" portNumber $ \ pending -> do
     sock <- WS.acceptRequest pending
     putStrLn "Received a websocket connection."
-    forever $ do
-        WS.sendTextData sock ("Pulse.\n"::L.ByteString)
-        delayMs 1000
+    forM_ [1..] $ \i -> do
+        WS.sendTextData sock ("Pulse: " <> (lbs $ show i) <> "\n")
+        delayMs 10
 
 -- https://developer.mozilla.org/en-US/docs/WebSockets/Writing_WebSocket_client_applications
 setupHeartBeatClient = [jmacro|
 var sock = new WebSocket(`heartBeatServerUrl::String`);
+var tableRoot = document.getElementById(`heartBeatTable::String`);
+var WIDTH = 10;
+var HEIGHT = 10;
+var cells = new Array(HEIGHT);
+for(var y=0; y<HEIGHT; y++) {
+    var row = document.createElement('tr');
+    cells[y] = new Array(WIDTH);
+    tableRoot.appendChild(row)
+    for(var x=0; x<WIDTH; x++) {
+        cells[y][x] = document.createElement('td');
+        cells[y][x].textContent = 'blank-ish';
+        row.appendChild(cells[y][x]);
+    }
+}
+var xIdx = 0;
+var yIdx = 0;
 sock.onmessage = function(event) {
-    var hbDiv = document.getElementById(`heartBeatDiv::String`);
+    /*var hbDiv = document.getElementById(`heartBeatDiv::String`);
     var x = document.createTextNode(event.data);
-    hbDiv.appendChild(x);
+    hbDiv.appendChild(x);*/
+    var node = cells[yIdx][xIdx];
+    node.textContent = event.data;
+    if(++xIdx >= WIDTH) {
+        xIdx = 0;
+        if(++yIdx >= HEIGHT) {
+            yIdx = 0;
+        }
+    }
 }
 |]
 
@@ -85,7 +112,8 @@ page = H.docTypeHtml $ do
         H.div H.! A.id escapingDiv $ do
             H.string "<p>testing escaping</p>" <> H.br
             H.string "<script>alert(\"XSS1\");</script>" <> H.br
-        mapM_ (\name -> H.pre $ H.div "" H.! A.id name) [factorialsDiv, heartBeatDiv]
+        H.pre $ H.div "" H.! A.id factorialsDiv
+        H.pre $ H.div H.! (A.id heartBeatDiv) $ H.table "" H.! (A.id heartBeatTable)
 
 pageServer request respond =
     respond $ responseLBS status200 [] $ renderHtml page
