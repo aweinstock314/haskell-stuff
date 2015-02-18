1c7e4ef5c58a3995ab11b5b3ec164b9fc1254231
Avi Weinstock <aweinstock314@gmail.com>
2015-02-18 10:04:02 -0500

    Added graph-drawing to MusicTest.

--- MusicTest.hs
+++ MusicTest.hs
@@ -46,7 +46,7 @@ adsr attack decay sustain release sample =
     step (i, x)           = (relVolume i) * x
     [atk, dec, rel] = map (*len) [attack, decay, release]
     atkVolume i = (i/atk)
-    decVolume i = 1-((i-atk)/(dec-atk))
+    decVolume i = (1-sustain)*(1-((i-atk)/(dec-atk)))+sustain
     relVolume i = sustain * (1-((i-rel)/(len-rel)))
     len = genericLength sample
 -- Eyeball test of envelope generator: zip [0..] $ adsr 0.1 0.3 0.5 0.9 $ replicate 20 1.0
@@ -60,9 +60,11 @@ tune1 sampleRate = execWriter $ do
         tell . env $ sineWave sampleRate 0.2 freq
         tell $ silence sampleRate 0.05
 
+linearCombination coeffs datums = foldl1 (zipWith (+)) $ zipWith (map . (*)) coeffs datums
+
 -- Meant to approximate tetris music.
-tune2 sampleRate = execWriter $ do
-    let env = map (*0.5) . adsr 0.0 0.9 0.25 0.95
+tune2 pitchShift sampleRate = execWriter $ do
+    let env = map (*0.5) . adsr 0.0 0.8 0.25 0.95
     let rest = tell . silence sampleRate
     let (d1, d2, d3, d4) = (0.15/2, 0.20, 0.225, 0.25)
     let f (freq, delay, emphasize) = do
@@ -70,12 +72,14 @@ tune2 sampleRate = execWriter $ do
         tell . map (*volumeMult) . env $ sineWave sampleRate (delay+0.15) freq
         rest delay
     let part1 = do
-        mapM_ f [(850, d2, True),
+        mapM_ f $ map (\(x,y,z) -> (x+pitchShift,y,z)) [(850, d2, True),
                 (700, d1, False), (750, d1, False), (800, d2, True), (750, d1, False), (700, d1, False), (650, d3, True),
                 (700, d1, False), (750, d1, False), (850, d2, True), (800, d1, False), (750, d1, False), (750, d2, True),
                 (700, d1, False), (750, d1, False), (800, d3, True), (850, d2, False), (800, d2, False),
                 (725, d2, True), (725, d4, True)]
     part1
+
+tune3 sampleRate = linearCombination [0.75, 0.25] [tune2 0 sampleRate, tune2 (-250) sampleRate]
 
 tuneServer tune = withAllWebsocketConnections $ \sock -> do
     sampleRateString <- WS.receiveData sock
@@ -95,6 +99,28 @@ function playBuffer(buf) {
     source.buffer = audioBuffer;
     source.connect(ctx.destination);
     source.start();
+}
+
+function canvasOfBuffer(buf, width, height, bgcolor, fgcolor) {
+    var scaleFactor = width/buf.length;
+    var canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    if(!canvas.getContext) { return; }
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = bgcolor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    var mid = height/2;
+    ctx.beginPath();
+    ctx.strokeStyle = fgcolor;
+    ctx.moveTo(0, mid);
+    //for(var i=0; i<buf.length; i++) { ctx.lineTo(i*scaleFactor, buf[i]*mid + mid); }
+    for(var i=0; i<canvas.width; i++) { ctx.lineTo(i, buf[Math.floor(i/scaleFactor)]*mid + mid); }
+    ctx.stroke();
+
+    return canvas;
 }
 
 function !playSineScale() {
@@ -120,6 +146,7 @@ function !playBufferFromServer() {
     sock.onmessage = function(event) {
         var buf = new Float32Array(event.data);
         playBuffer(buf);
+        document.body.appendChild(canvasOfBuffer(buf, 9973, 500, 'rgb(200,200,200)', 'black'));
     };
 }
 |]
@@ -129,12 +156,12 @@ page = H.docTypeHtml $ do
         H.title "Music Test"
         embedScriptMultiline jsDefinitions
     H.body `onloadDo` [jmacroE|playBufferFromServer()|] $ do
-        "Audio should be playing."
+        "Audio should start playing once the graph appears:"
 
 pageServer request respond = respond $ responseLBS status200 [] $ renderHtml $ page
 
 main = do
     let portNumber = 8504
     putStrLn $ mconcat ["Listening on port ", show portNumber, "."]
-    forkIO $ tuneServer tune2 (portNumber+1)
+    forkIO $ tuneServer (tune2 0) (portNumber+1)
     run portNumber pageServer
