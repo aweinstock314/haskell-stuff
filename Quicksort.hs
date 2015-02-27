7ed527574c0c5e5a3dc4eb431a8ecd634e8ef9cd
Avi Weinstock <aweinstock314@gmail.com>
2015-02-27 06:08:02 -0500

    Added simpler quicksort partitioning algorithm with better worst-case performance.

--- Quicksort.hs
+++ Quicksort.hs
@@ -8,6 +8,8 @@ import qualified Data.Vector.Mutable as VM
 import Data.List
 import Test.QuickCheck
 
+{-
+-- http://en.wikipedia.org/wiki/Quicksort
 partitionByST cmp vec lo hi = do
     let pivotIdx = (lo + hi) `div` 2
     pivotVal <- VM.read vec pivotIdx
@@ -26,6 +28,24 @@ quicksortByST cmp vec = aux 0 (VM.length vec - 1) where
         mid <- partitionByST cmp vec lo hi
         aux lo (mid-1)
         aux (mid+1) hi
+-}
+
+-- http://en.wikipedia.org/wiki/Dutch_national_flag_problem
+partitionByST cmp vec lo hi = VM.read vec pivotIdx >>= aux lo lo hi where
+    pivotIdx = lo + ((hi-lo) `div` 2)
+    aux i j n mid | j <= n = do
+        a_j <- VM.read vec j
+        case cmp a_j mid of
+            LT -> VM.swap vec i j >> aux (i+1) (j+1) n mid
+            GT -> VM.swap vec j n >> aux i j (n-1) mid
+            EQ -> aux i (j+1) n mid
+    aux i j n mid = return (i, j)
+
+quicksortByST cmp vec = aux 0 (VM.length vec - 1) where
+    aux lo hi = when (lo < hi) $ do
+        (leftMid, rightMid) <- partitionByST cmp vec lo hi
+        aux lo leftMid
+        aux rightMid hi
 
 quicksortBy cmp vec = V.modify (quicksortByST cmp) vec
 
